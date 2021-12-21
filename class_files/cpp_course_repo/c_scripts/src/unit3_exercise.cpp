ae129c92595916aad063372511735c2465c2b4c6
Balakrishnan Irudhayaraman <balakrish1712@gmail.com>
2021-12-21 20:14:13 +0100

    unit3 exercise moving the robot from one place of the world to the other with alternate forward and rotation movements

--- class_files/cpp_course_repo/c_scripts/src/unit3_exercise.cpp
+++ class_files/cpp_course_repo/c_scripts/src/unit3_exercise.cpp
@@ -1,11 +1,17 @@
 #include "rosbot_control/rosbot_class.h"
 #include <ros/ros.h>
 #include <list>
+#include <string>
 
 
 
-std::list<float> move_and_inform(RosbotClass& rosbot, int n_secs){
+std::list<float> move_and_inform(RosbotClass& rosbot, int n_secs, std::string rot_direction){
   rosbot.move_forward(n_secs);
+  rosbot.turn(rot_direction,n_secs);
+  rosbot.move_forward(2);
+  rosbot.turn(rot_direction,2);
+  rosbot.move_forward(2);
+ 
   auto x = rosbot.get_position(1);
   auto y = rosbot.get_position(2);
   return std::list<float>{x,y};
@@ -15,7 +21,7 @@ int main(int argc, char **argv) {
 
   RosbotClass rosbot;
   
-  auto pose_after_move = move_and_inform(rosbot,3);
+  auto pose_after_move = move_and_inform(rosbot,3,"clockwise");
   ROS_INFO_STREAM("Position after moving....");
   for (auto val:pose_after_move){
     ROS_INFO_STREAM(val);
