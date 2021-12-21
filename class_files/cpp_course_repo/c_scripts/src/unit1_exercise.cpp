a0a39db47a6ece93598a59ed59044d56f6dc5628
Balakrishnan Irudhayaraman <balakrish1712@gmail.com>
2021-12-21 18:21:13 +0100

    Change in the code of unit1_exercise.cpp to check and print the vlocity along x<1m/s

--- class_files/cpp_course_repo/c_scripts/src/unit1_exercise.cpp
+++ class_files/cpp_course_repo/c_scripts/src/unit1_exercise.cpp
@@ -5,25 +5,28 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "rosbot_node");
 
-  std::map<double, float> robot_positions;
+  //std::map<double, float> robot_positions;
   RosbotClass rosbot;
   rosbot.move();
 
-  auto x1 = rosbot.get_position(1);
-  auto timestamp = rosbot.get_time();
+  auto x_start = rosbot.get_position(1);
+  auto timestamp_start = rosbot.get_time();
 
-  robot_positions.insert(std::make_pair(timestamp,x1));
+  //robot_positions.insert(std::make_pair(timestamp,x1));
 
   rosbot.move();
 
-  x1 = rosbot.get_position(1);
-  timestamp = rosbot.get_time();
+  auto x_end = rosbot.get_position(1);
+  auto timestamp_end = rosbot.get_time();
 
-  robot_positions.insert(std::make_pair(timestamp,x1));
+  auto speed = (x_end - x_start)/(timestamp_end-timestamp_start);
 
-  for (auto& [key,value]:robot_positions){
+  ROS_INFO_STREAM("Is Velocity along x-axis less than 1m/s?\n"<<(speed<1)<<"\n");
+  //robot_positions.insert(std::make_pair(timestamp,x1));
+
+/*  for (auto& [key,value]:robot_positions){
     ROS_INFO_STREAM("X: "<<value<<" at timestamp ["<<key<<"]\n");
-  }
+  }*/
 
   return 0;
 }
