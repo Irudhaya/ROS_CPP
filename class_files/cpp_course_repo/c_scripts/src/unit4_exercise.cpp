51f8eaaef0466142184fa039eef29c6daa3a4b83
Balakrishnan Irudhayaraman <balakrish1712@gmail.com>
2021-12-21 22:01:34 +0100

    unit4 exercise to receive the laser readings and print all the laser readings by accessing through pointers

--- class_files/cpp_course_repo/c_scripts/src/unit4_exercise.cpp
+++ class_files/cpp_course_repo/c_scripts/src/unit4_exercise.cpp
@@ -14,6 +14,19 @@ int main(int argc, char **argv) {
   ROS_INFO_STREAM("RObot is "<<laser_readings[0]<<" from the right wall....\n");
   ROS_INFO_STREAM("RObot is "<<laser_readings[1]<<" from the left wall....\n");
   
+  float* laser_values_start;
+  laser_values_start = rosbot.get_laser_full();
 
+  auto laser_values_end = laser_values_start + 720;
+  //ROS_INFO_STREAM(laser_values_start+720);
+  ros::Duration(1).sleep();
+  ROS_INFO_STREAM("The lase values are....");
+  int i{0};
+  while(laser_values_start!=laser_values_end){
+  	//ROS_INFO_STREAM(laser_values_start<<","<<laser_values_end<<"---> Counter: "<<i++<<"\n");
+  	ROS_INFO_STREAM(*laser_values_start<<"--> Counter: "<<++i<<"\n");
+  	laser_values_start++;
+  	ros::Duration(0.1).sleep();
+  }
   return 0;
 }
