e7d0a4c09b8fc8f70153b59b62df99d849fa34de
Balakrishnan Irudhayaraman <balakrish1712@gmail.com>
2021-12-21 19:04:21 +0100

    moving the robot continuously is the desired limit is not reached else to stop the robot

---
+++ class_files/cpp_course_repo/c_scripts/src/unit2_exercise.cpp
@@ -0,0 +1,21 @@
+#include "rosbot_control/rosbot_class.h"
+#include <ros/ros.h>
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "rosbot_node");
+
+  RosbotClass rosbot;
+  
+  rosbot.move_forward(5);
+
+  auto x = rosbot.get_position(1);
+
+  while (x<1.0){
+    rosbot.move_backwards(2);
+  }
+
+  rosbot.stop_moving();
+
+  return 0;
+}
